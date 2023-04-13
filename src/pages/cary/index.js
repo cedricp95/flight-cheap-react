d82fa65e5dc4e46731f0479d167bd53ea9acb5d7
Cary <caryb@codev.com>
2023-04-13 20:53:41 +0800

    added grid profiles

--- src/pages/cary/index.js
+++ src/pages/cary/index.js
@@ -1,5 +1,6 @@
 import MeetupList from "../../components/meetups/MeetupList";
-import { Button } from "@mui/material";
+import { Button, Typography, Grid, Box, Avatar } from "@mui/material";
+import Image from "next/image";
 
 const DUMMY_MEETUPS = [
   {
@@ -23,10 +24,48 @@ const DUMMY_MEETUPS = [
 function HomePage() {
   return (
     <>
-      <MeetupList meetups={DUMMY_MEETUPS} />
-      <Button variant="contained" size="large">
-        Sample UI Button
-      </Button>
+      <div className="center">
+        <MeetupList meetups={DUMMY_MEETUPS} />
+        <Typography variant="h1">
+          This is a sample text with typography
+        </Typography>
+        <Button variant="contained" size="large">
+          Sample UI Button
+        </Button>
+      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            "--Grid-borderWidth": "1px",
+            borderTop: "var(--Grid-borderWidth) solid",
+            borderLeft: "var(--Grid-borderWidth) solid",
+            borderColor: "divider",
+            "& > div": {
+              borderRight: "var(--Grid-borderWidth) solid",
+              borderBottom: "var(--Grid-borderWidth) solid",
+              borderColor: "divider",
+            },
+          }}
+        >
+          {["Jimmy", "Michal", "Jun", "Marija"].map((name, index) => (
+            <Grid
+              key={name}
+              xs={12}
+              sm={6}
+              md={3}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              minHeight={180}
+            >
+              <Avatar src={`/about-us/avatar${index + 1}.jpg`} size="lg" />
+              <Typography sx={{ ml: 1.5 }}>{name}</Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 }
