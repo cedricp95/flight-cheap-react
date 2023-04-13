9db4ae4a5bee0072c9ab3851d7c70ad1f19027c3
Cary <caryb@codev.com>
2023-04-13 10:52:05 +0800

    send rest api working example (design wip)

---
+++ src/pages/cary/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
