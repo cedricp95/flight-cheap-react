20d8715dafe41f7fef32020e787085c4949ad4d5
Cary <caryb@codev.com>
2023-04-20 07:56:47 +0800

    about_us: remove cards, added bottom padding on avatar for footer

--- src/pages/about_us/index.js
+++ src/pages/about_us/index.js
@@ -134,13 +134,12 @@ const AboutUs = () => {
       <Grid
         sx={{
           textAlign: "center !important",
-          justifyContent: "center !important",
-          alignItems: "center !important",
         }}
         container
         spacing={2}
         pl={8}
         pr={8}
+        pb={8}
       >
         {devTeam.map((persons, index) => (
           <Grid item xs={12} sm={4} key={persons.id}>
@@ -161,45 +160,6 @@ const AboutUs = () => {
         ))}
       </Grid>
       {/* End of Avatar */}
-
-      <Container sx={{ py: 8 }} maxWidth="md">
-        <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  sx={{
-                    // 16:9
-                    pt: "56.25%",
-                  }}
-                  image="https://source.unsplash.com/random"
-                  alt="random"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading 1
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe
-                    the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Edit</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
     </Layout>
   );
 };
