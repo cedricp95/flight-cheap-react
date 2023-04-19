419b32e7413feb890ff33bfc6d691e1be01cb094
Cary <caryb@codev.com>
2023-04-19 09:56:23 +0800

    about_us: work in progress separation of header & footer component

--- src/pages/about_us/index.js
+++ src/pages/about_us/index.js
@@ -24,7 +24,7 @@ import {
   Paper,
 } from "@mui/material";
 
-const FlightCheapLogoOnlyTransparent = "/FlightCheapLogoOnlyTransparent.png";
+import Footer from "./../../components/Footer";
 
 function Copyright() {
   return (
@@ -52,189 +52,177 @@ const cards = [1, 2, 3];
 const theme = createTheme();
 
 const AboutUs = () => {
+  const FlightCheapLogoOnlyTransparent = "/FlightCheapLogoOnlyTransparent.png";
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
-            About Us
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: "background.paper",
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Album layout
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <CameraIcon sx={{ mr: 2 }} />
+            <Typography variant="h6" color="inherit" noWrap>
+              About Us
             </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
-          </Container>
-        </Box>
-        {/* End hero unit */}
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Our Goal
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="text.secondary"
+                paragraph
+              >
+                To provide affordable flight fares to our customers without
+                compromising on the quality of service and convenience, making
+                air travel accessible and budget-friendly for everyone.
+              </Typography>
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+              >
+                <Button variant="contained">Main call to action</Button>
+                <Button variant="outlined">Secondary action</Button>
+              </Stack>
+            </Container>
+          </Box>
+          {/* End hero unit */}
 
-        {/* Avatar */}
-        <Box
-          sx={{
-            bgcolor: "background.paper",
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="md">
-            <Grid
-              container
-              spacing={2}
-              sx={{
-                "--Grid-borderWidth": "1px",
-                borderTop: "var(--Grid-borderWidth) solid",
-                borderLeft: "var(--Grid-borderWidth) solid",
-                borderColor: "divider",
-                "& > div": {
-                  borderRight: "var(--Grid-borderWidth) solid",
-                  borderBottom: "var(--Grid-borderWidth) solid",
-                  borderColor: "divider",
-                },
-              }}
-            >
-              {[
-                "Starsky",
-                "Cedric",
-                "Cary",
-                "Francely",
-                "Paul",
-                "Romart",
-                "Dan",
-                "Romulo",
-              ].map((name, index) => (
-                <Grid
-                  key={name}
-                  xs={12}
-                  sm={6}
-                  md={3}
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  minHeight={180}
-                >
-                  <Avatar
-                    src={`/about-us/avatar${index + 1}.jpg`}
-                    sx={{ width: 125, height: 125 }}
-                  />
+          {/* Avatar */}
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="md">
+              <Grid container spacing={2}>
+                {[
+                  "Starsky",
+                  "Cedric",
+                  "Cary",
+                  "Francely",
+                  "Paul",
+                  "Romart",
+                ].map((name, index) => (
+                  <Grid
+                    key={name}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    minHeight={180}
+                  >
+                    <Avatar
+                      src={`/about-us/avatar${index + 1}.jpg`}
+                      sx={{ width: 125, height: 125 }}
+                    />
 
-                  <Typography sx={{ ml: 1.5 }}>{name}</Typography>
+                    <Typography sx={{ ml: 1.5 }}>{name}</Typography>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          </Box>
+          {/* End of Avatar */}
+
+          {/* Cards */}
+          <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: "56.25%",
+                      }}
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to
+                        describe the content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">View</Button>
+                      <Button size="small">Edit</Button>
+                    </CardActions>
+                  </Card>
                 </Grid>
               ))}
             </Grid>
           </Container>
-        </Box>
-        {/* End of Avatar */}
+          {/* Cards */}
 
-        {/* Cards */}
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: "56.25%",
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-        {/* Cards */}
-      </main>
-      {/* Footer */}
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <Image
-          src={FlightCheapLogoOnlyTransparent}
-          alt="Cheap Flight Logo"
-          width={50}
-          height={50}
-        />
-      </Box>
+          {/* Footer */}
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Image
+              src={FlightCheapLogoOnlyTransparent}
+              alt="Cheap Flight Logo"
+              width={50}
+              height={50}
+            />
+          </Box>
 
-      <Box
-        sx={{ bgcolor: "background.paper", p: 1 }}
-        component="footer"
-        alignContent="flex-start"
-        alignItems="flex-start"
-        justify="flex-start"
-      >
-        <Typography variant="h6" align="center" gutterBottom>
-          FlightCheap
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Cheapest Flight Fare Finder
-        </Typography>
-        <Copyright />
-      </Box>
-      {/* End footer */}
-    </ThemeProvider>
+          <Box
+            sx={{ bgcolor: "background.paper", p: 1 }}
+            component="footer"
+            alignContent="flex-start"
+            alignItems="flex-start"
+            justify="flex-start"
+          >
+            <Typography variant="h6" align="center" gutterBottom>
+              FlightCheap
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              align="center"
+              color="text.secondary"
+              component="p"
+            >
+              Cheapest Flight Fare Finder
+            </Typography>
+            <Copyright />
+          </Box>
+          {/* End Footer */}
+        </main>
+      </ThemeProvider>
+    </>
   );
 };
 
