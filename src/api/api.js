0b780dc7fafc1626da0b7cd97d1e92a79f72c008
pauly-codev <pauly@codev.com>
2023-04-23 21:49:02 +0800

    Implement API

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,35 @@
 import axios from 'axios';
 
+const list_except_path_auth = [
+  '/login',
+  '/healthz'
+]
 export default function handler() {
-  
+  const instance = axios.create({
+    baseURL: 'https://squid-app-9q7r5.ondigitalocean.app',
+    withCredentials:true
+    //headers: {"authentication":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiZXhwIjoxNjgyMjAzOTI3fQ.8LfVp_d8q14QPYLHYV3Rea_Vxl4tsWA03cRv2FiC8mo"}
+  });
   // Add a request interceptor
-  axios.interceptors.request.use(function (config) {
+  instance.interceptors.request.use(function (config) {
     // Do something before request is sent
-    console.log(config,"request")
+    //console.log(config.headers,"headers")
+    if (list_except_path_auth.indexOf(config.url) === -1){
+      const token = sessionStorage.getItem("token"); 
+      //console.log(config,"request",config.url)
+      
+      //config.headers['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+      if (token !== null){
+           config.headers['Bearer-Authentication'] = token;
+       // instance.defaults.headers.common['Bearer-Authentication'] = token;
+
+        console.log( token,"::token",config)
+        return config;
+      }else{
+        alert("Session has expired, please try to login again")
+      }
+    }
+    
     return config;
   }, function (error) {
     // Do something with request error
@@ -13,7 +37,7 @@ export default function handler() {
   });
 
   // Add a response interceptor
-  axios.interceptors.response.use(function (response) {
+  instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     console.log(response,"response")
@@ -24,8 +48,6 @@ export default function handler() {
     return Promise.reject(error);
   });
   //const instance = axios.default.baseUrl = "http://localhost:5000";
-  const instance = axios.create({
-    baseURL: 'http://localhost:5000'
-  });
+ 
   return instance;
 }
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,6 +1,17 @@
 import Layout from '../../components/layout/Layout' 
+import {healthz,get_iata} from '../../api/auth';
+import React, { useState,useEffect } from 'react';
 
 export default function Dashboard() {
+  useEffect(() => {
+    
+    get_iata().then((res,req)=>{
+      
+      console.log(res,"res",req);
+    }).catch((e)=>{
+    })
+  });
+
   return (
     <>
       <Layout type="auth">
