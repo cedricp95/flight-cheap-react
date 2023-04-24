b812b2e5f30bf45dd33f0070bc59988bc2fb441e
Cary <caryb@codev.com>
2023-04-24 19:22:59 +0800

    dashboard: applied auto formatting

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,19 +1,16 @@
-import api from './api';
+import api from "./api";
 
 export async function healthz() {
-
-    return await api().get("/healthz")
+  return await api().get("/healthz");
 }
 
-export async function login(username,password) {
-
-    return await api().post("/login", {
-        username:username,
-        password:password
-    })
+export async function login(username, password) {
+  return await api().post("/login", {
+    username: username,
+    password: password,
+  });
 }
 
 export async function get_iata() {
-
-    return await api().get("/iata_code/")
+  return await api().get("/iata_code/");
 }
