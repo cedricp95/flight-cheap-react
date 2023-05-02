e81f35255d6fbc40879b8dd4355edd20c52ecd72
Cary <caryb@codev.com>
2023-05-03 07:03:16 +0800

    api: change baseURL of the api

--- src/api/api.js
+++ src/api/api.js
@@ -1,49 +1,52 @@
-import axios from 'axios';
+import axios from "axios";
 
-const list_except_path_auth = [
-  '/login',
-  '/healthz'
-]
+const list_except_path_auth = ["/login", "/healthz"];
 export default function handler() {
   const instance = axios.create({
-    baseURL: 'https://squid-app-9q7r5.ondigitalocean.app',
-    withCredentials:true
+    baseURL: "http://188.166.133.135",
+    withCredentials: true,
   });
   // Add a request interceptor
-  instance.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    if (list_except_path_auth.indexOf(config.url) === -1){
-      const token = sessionStorage.getItem("token"); 
-        if (token !== null){
-           config.headers['Bearer-Authentication'] = token;
-        return config;
-      }else{
-        window.sessionStorage.clear();
-        alert("Session has expired, please try to login again")
+  instance.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      if (list_except_path_auth.indexOf(config.url) === -1) {
+        const token = sessionStorage.getItem("token");
+        if (token !== null) {
+          config.headers["Bearer-Authentication"] = token;
+          return config;
+        } else {
+          window.sessionStorage.clear();
+          alert("Session has expired, please try to login again");
+        }
       }
+
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
     }
-    
-    return config;
-  }, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  });
+  );
 
   // Add a response interceptor
-  instance.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    if (error.response.data.detail==="X-Token header expired"){
-      window.sessionStorage.clear();
-      window.location.reload();
+  instance.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      if (error.response.data.detail === "X-Token header expired") {
+        window.sessionStorage.clear();
+        window.location.reload();
+      }
+      return Promise.reject(error);
     }
-    return Promise.reject(error);
-  });
+  );
   //const instance = axios.default.baseUrl = "http://localhost:5000";
- 
+
   return instance;
 }
