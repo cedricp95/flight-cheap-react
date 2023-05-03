2affb163098dd3b1fc49425a8495d0ff06963e9d
Cary <caryb@codev.com>
2023-05-03 08:48:05 +0800

    refactor: removed unused folder

--- src/pages/api/comments/[commentId].js
+++
@@ -1,9 +0,0 @@
-import { comments } from "../../../data/comments";
-
-export default function handler(req, res) {
-  const { commentId } = req.query;
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(commentId)
-  );
-  res.status(200).json(comment);
-}
